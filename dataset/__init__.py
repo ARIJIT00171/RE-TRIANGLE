b6300b75527752544892df411256a46a711e4141
CSBindra <csinghbindra@gmail.com>
2025-12-31 16:10:27 +0530

    Removed duplicate dataset folder

--- dataset/__init__.py
+++
@@ -1,7 +0,0 @@
-from dataset.index_anno import AnnoIndexedDataset
-from dataset.index_src import SrcIndexedDataset
-
-dataset_registry = {
-    "annoindexed": AnnoIndexedDataset,
-    "srcindexed": SrcIndexedDataset,
-}
